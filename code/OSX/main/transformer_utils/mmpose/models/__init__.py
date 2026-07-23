f399b99486623ac94abccaf6b243a22c02f8e78f
Aaron J. <jdaaron123@gmail.com>
2026-07-23 12:12:42 -0500

    Avoid eager Poseur import in vendored mmpose

--- code/OSX/main/transformer_utils/mmpose/models/__init__.py
+++ code/OSX/main/transformer_utils/mmpose/models/__init__.py
@@ -2,11 +2,6 @@
 from .builder import (BACKBONES, HEADS, LOSSES, MESH_MODELS, NECKS, POSENETS,
                       build_backbone, build_head, build_loss, build_mesh_model,
                       build_neck, build_posenet)
-from .detectors import *  # noqa
-from .heads import *  # noqa
-from .losses import *  # noqa
-from .necks import *  # noqa
-from .utils import *  # noqa
 
 
 __all__ = [
